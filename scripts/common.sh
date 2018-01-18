e6e41329a323afadb9693d01bcd14b15408ad627
Jonathon Hill <jhill@thoughtworks.com>
2018-01-18 14:19:04 +1100

    [SSL][Jono] attempt 4 to fix update cert issue

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function setup_gcloud() {
-  gcloud auth activate-service-account --key-file /root/gcp-key.json
+  gcloud auth activate-service-account --key-file /root/tmp/gcp-key.json
   gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
   gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
   gcloud --quiet config set container/cluster ${GOOGLE_CLUSTER_NAME}

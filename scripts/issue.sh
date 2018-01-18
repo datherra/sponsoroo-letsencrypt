7a4e92d06180ed830dac1d6ed7ab9bc343428fd5
Jonathon Hill <jhill@thoughtworks.com>
2018-01-18 13:02:15 +1100

    [Jono] issue using gsutil in docker container with google auth service account solved

--- scripts/issue.sh
+++ scripts/issue.sh
@@ -6,6 +6,7 @@ source common.sh
 
 BUCKET=tw-certs-sponsoroo
 
+gcloud config set pass_credentials_to_gsutil false
 ensure_bucket_exists "$BUCKET"
 download_previous_run_data "$BUCKET" "$FQDN"-keys.tgz
 run_acme_client "$LETSENCRYPT_API" "$FQDN"
